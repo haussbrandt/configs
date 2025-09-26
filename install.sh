75527e062c1d0cf3b54f9db01abf365681bc44d8
Daniel Kupnicki <haussbrandt@gmail.com>
2025-09-26 21:52:56 +0200

    nvim plugins

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@ cp .tmux.conf ~
 
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt install -y make gcc ripgrep unzip git xclip neovim
+sudo apt install -y make gcc ripgrep unzip git xclip neovim fd-find
+ln -s $(which fdfind) ~/.local/bin/fd
 
 mkidr -p "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 cp init.lua "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+cp -r lua "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/lua
 
