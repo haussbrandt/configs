bd49be6ebc5344eb3424d171d5014ecac1fcb5ea
Daniel Kupnicki <haussbrandt@gmail.com>
2025-10-02 21:57:08 +0200

    add mini.files for navigating the files system

--- init.lua
+++ init.lua
@@ -834,6 +834,7 @@ require("lazy").setup({
 			-- - sd'   - [S]urround [D]elete [']quotes
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require("mini.surround").setup()
+			require("mini.files").setup()
 
 			-- Simple and easy statusline.
 			--  You could remove this setup call if you don't like it,
