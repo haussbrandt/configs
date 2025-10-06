e7afc01dc7dcaf5f100aea818aa1054d4493774d
haussbrandt <haussbrandt@gmail.com>
2025-10-06 16:06:45 +0200

    Fix typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y make gcc ripgrep unzip git xclip neovim fd-find
 ln -s $(which fdfind) ~/.local/bin/fd
 
-mkidr -p "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 cp init.lua "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 cp -r lua "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim/lua
 
